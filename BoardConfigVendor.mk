9e7928e8dd445d7a79732de662b0fd0290c551f4
zainarbani <zaintsyariev@gmail.com>
2022-08-04 10:20:26 +0700

    Add prebuilt firmware
     * from pissarro-user-11-RP1A.200720.011-V12.5.7.0.RKTMIXM-release-keys
    
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>

---
+++ BoardConfigVendor.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2021 Raphielscape LLC. and Haruka LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# AB Paritions
+AB_OTA_PARTITIONS += \
+    audio_dsp \
+    cam_vpu1 \
+    cam_vpu2 \
+    cam_vpu3 \
+    dpm \
+    gz \
+    lk \
+    mcupm \
+    md1img \
+    odm \
+    pi_img \
+    preloader_raw \
+    scp \
+    spmfw \
+    sspm \
+    tee
